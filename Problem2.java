084689fcb2d8b76625bb3dcccab79e8548ff0dda
Tripti Ghanghas <triptighanghas23@gmail.com>
2025-01-18 03:19:36 -0800

    problem 2 done

---
+++ Problem2.java
@@ -0,0 +1,22 @@
+//TC: O(n*capacity)
+//SC: O(n*capacity)
+//approach: dynamic programming
+
+class Knapsack {
+    static int knapSack(int capacity, int val[], int wt[]) {
+        int n = wt.length;
+        int[][] dp = new int[n + 1][capacity + 1];
+
+        for (int i = 0; i <= n; i++) {
+            for (int w = 0; w <= capacity; w++) {
+                if (i == 0 || w == 0)
+                    dp[i][w] = 0;
+                else if (wt[i - 1] <= w)
+                    dp[i][w] = Math.max(val[i - 1] + dp[i - 1][w - wt[i - 1]], dp[i - 1][w]);
+                else
+                    dp[i][w] = dp[i - 1][w];
+            }
+        }
+        return dp[n][capacity];
+    }
+}
